b75175c402dde77ee4bdec647115862d6cfe2d0b
PtitLuca <luca.georges-francois@epitech.eu>
2020-11-16 00:57:58 +0100

    [+] - Add deployment on a local network test.

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -9,16 +9,25 @@ const provider = ganache.provider();
 const Web3 = require('web3');
 // web3 will use the ganache provider for testing purposes.
 const web3 = new Web3(provider);
+// To make the tests.
+const assert = require('assert');
 
 describe('Inbox', () => {
   let accounts = undefined;
+  let contract = undefined;
+  let initialMsg = 'Hello World !';
 
   beforeEach(async () => {
     // Retrieve all ethereum accounts currently loaded.
     accounts = await web3.eth.getAccounts();
+
+    // Deploy the contract is required to interact with it.
+    contract = await new web3.eth.Contract(abi)
+      .deploy({ data: bytecode.object, arguments: [initialMsg] })
+      .send({ from: accounts[0], gas: 1000000 });
   });
 
   it('can deploy a contract', () => {
-
+    assert.ok(contract.options.address);
   });
 });
